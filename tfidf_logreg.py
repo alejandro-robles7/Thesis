730b1d5b23954bf795616b29f464007126a8bbe5
Alejandro Robles <alejandro.robles@thetradedesk.com>
2019-07-13 16:13:58 -0700

    log reg changes

--- tfidf_logreg.py
+++ tfidf_logreg.py
@@ -1,11 +1,11 @@
-#Training an SVM on TF-IDF vectors
+#Training an Logistic Regression Model on TF-IDF vectors
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.pipeline import Pipeline
-from sklearn.linear_model import SGDClassifier, LogisticRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfTransformer
 
 csv_path_cleaned = 'files/data_cleaned.txt'
@@ -16,21 +16,16 @@ y = df['Category']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state = 42)
 
-#linear classifier (SVM)
-
-#this is the pipeline transforming the input data
-#CountVectorizer and TfidfTransformer are used for feature extraction
-#The SGDClassifier class creates the SVM model
-sgd_linear = Pipeline((('vect', CountVectorizer()),
+logreg_model = Pipeline((('vect', CountVectorizer()),
                        ('tfidf', TfidfTransformer()),
                        ('clf',
                         LogisticRegression(multi_class='multinomial', solver='lbfgs'
                         ))))
 
 #train the model
-sgd_linear.fit(X_train, y_train)
+logreg_model.fit(X_train, y_train)
 
-y_pred_linear = sgd_linear.predict(X_test)
+y_pred_linear = logreg_model.predict(X_test)
 
 print('accuracy linear model %s' % accuracy_score(y_pred_linear, y_test))
 
