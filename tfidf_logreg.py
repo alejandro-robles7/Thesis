c8c4fc5866b10a5be56250cfe38fa573d61e11cc
Alejandro Robles <alejandro.robles@thetradedesk.com>
2019-07-16 19:59:38 -0700

    TF-IDF Binary

--- tfidf_logreg.py
+++ tfidf_logreg.py
@@ -1,32 +1,40 @@
 #Training an Logistic Regression Model on TF-IDF vectors
 
-import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import accuracy_score, classification_report
 from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfTransformer
 
-csv_path_cleaned = 'files/data_cleaned.txt'
+from glove_vectors_logreg import *
 
-df = pd.read_csv(csv_path_cleaned, header = None, names = ['Category', 'Text'], sep =' ')
-X = df['Text']
-y = df['Category']
+def model_pipe(binary=True):
+    if binary:
+        model = Pipeline([('vect', CountVectorizer()),
+                          ('tfidf', TfidfTransformer()),
+                          ('clf', LogisticRegression()
+                           )])
+    else:
+        model = Pipeline([('vect', CountVectorizer()),
+                          ('tfidf', TfidfTransformer()),
+                          ('clf', LogisticRegression(multi_class='multinomial', solver='lbfgs')
+                           )])
+    return model
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state = 42)
+def train_pipeline(x, y, model):
+    X_train, X_test, y_train, y_test = split_dataset(x, y)
+    model = model.fit(X_train, y_train)
+    return calc_accuracy(model, X_test, y_test), model
 
-logreg_model = Pipeline((('vect', CountVectorizer()),
-                       ('tfidf', TfidfTransformer()),
-                       ('clf',
-                        LogisticRegression(multi_class='multinomial', solver='lbfgs'
-                        ))))
 
-#train the model
-logreg_model.fit(X_train, y_train)
+if __name__ == '__main__':
 
-y_pred_linear = logreg_model.predict(X_test)
+    csv_path_cleaned = 'files/data_cleaned.txt'
+    binary_flag = True
 
-print('accuracy linear model %s' % accuracy_score(y_pred_linear, y_test))
+    X, y = load_data(csv_path_cleaned)
 
-print("report linear model", classification_report(y_test, y_pred_linear,target_names=sgd_linear.classes_))
+    if binary_flag:
+        sports_ind, other_ind = binarize_ind(y, one_vs_all=True)
+        y = mask_label(y, other_ind)
+
+    logreg_model = model_pipe(binary_flag)
+    accuracy, model = train_pipeline(X, y, logreg_model)
